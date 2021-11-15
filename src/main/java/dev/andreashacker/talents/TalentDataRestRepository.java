372f4cc27f607f73df5db2c8ea453786afeab7a9
Andreas Hacker <hacker.andreas1@gmail.com>
2021-11-15 11:37:57 +0100

    added data.sql, changed id to Long

--- src/main/java/dev/andreashacker/talents/TalentDataRestRepository.java
+++ src/main/java/dev/andreashacker/talents/TalentDataRestRepository.java
@@ -3,8 +3,6 @@ package dev.andreashacker.talents;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.UUID;
-
 @RepositoryRestResource(collectionResourceRel = "talents")
-public interface TalentDataRestRepository extends PagingAndSortingRepository<Talent, UUID> {
+public interface TalentDataRestRepository extends PagingAndSortingRepository<Talent, Long> {
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into talent
+values(10001, 'Trevor Slattery');
+
+insert into talent
+values(10002, 'The actor formerly known as me');
