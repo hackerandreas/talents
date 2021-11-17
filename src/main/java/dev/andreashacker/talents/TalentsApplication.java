fbe8f99ba9732982eb0f5b921a8c9e9deae0ab78
Andreas Hacker <hacker.andreas1@gmail.com>
2021-11-17 12:15:32 +0100

    added swagger, junit test

--- src/main/java/dev/andreashacker/talents/TalentsApplication.java
+++ src/main/java/dev/andreashacker/talents/TalentsApplication.java
@@ -2,12 +2,13 @@ package dev.andreashacker.talents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TalentsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TalentsApplication.class, args);
     }
-
 }
